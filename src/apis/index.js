8ff71f8cab1695858ed80af2ebaa32954a6868c1
YiYidecat <YiYidecat@users.noreply.github.com>
2026-02-10 14:47:01 +0000

    deploy: a367f2beba3f9081c8b8bf2a78ae454d895967d8

--- src/apis/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * API接口统一导出
- */
-export { PostAPI } from './PostAPI.js'
-export { CategoryAPI } from './CategoryAPI.js'
-export { CommentAPI } from './CommentAPI.js'
-export { UserAPI } from './UserAPI.js'
